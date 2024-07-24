3803de0293a98e2407108858c4a61ba1a99ef336
Theo Labadie <theo.labadie@fms-ea.com>
2024-07-24 15:50:36 +0200

    add class product

---
+++ product.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+
+class Produit:
+    def __init__(self, name:str, type:str, stock:float, price:float):
+        self.name:str = name
+        self.type:str = type
+        self.stock:float = stock
+        self.price:float = price
+
+    def update_store(self, add_to_cart):
+        self.stock -= add_to_cart
+
+    def __str__(self):
+        return f"product(name={self.name}, type={self.type}, stock={self.stock}, prix={self.price})"
