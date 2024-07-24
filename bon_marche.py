5d9c7823a974478b442d1dda85b4626f30f3c892
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-24 20:24:58 +0200

    add function main

--- bon_marche.py
+++ bon_marche.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+
 # -*- coding: utf-8 -*-
-print('test')
-print("test2")
+import store
+from store import Store
+
+
+def main():
+     store= Store()
+     store.store_menu()
+     stock(store)
+
+if __name__=="__main__" :
+     main()
