9b9c93e14d7ed9573554f5a4ba21423797bed588
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-24 14:53:26 +0200

    add init de store

---
+++ store.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+from typing import  Dict, List
+from Product import  Product
+from Customers import  costomers
+ """
+
+ """
+ class Store:
+     def __init__(self):
+         self.products: Dict[str, Product]={}
+         self.customers: List[Customer]=[]
+     def add_product (self)
+
