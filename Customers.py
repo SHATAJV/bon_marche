30b8c7e0b70476ae6b5fdd6616998e7d1a21a7e2
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-25 08:10:55 +0200

    correctoin de function recipt

--- Customers.py
+++ Customers.py
@@ -10,6 +10,7 @@ class Customers:
         self.first_name:str = first_name
         self.last_name: str = last_name
         self.cart:list = []
+
     def add_to_cart(self,product:str,quantity:float):
         if product.update_store(quantity):
             self.cart.append(product,quantity)
@@ -20,8 +21,12 @@ class Customers:
         total_price = sum(product.price * quantity for product, quantity in self.cart)
         return total_price
 
-
     def receipt(self) -> str:
+        receipt = []
         for product, quantity in self.cart:
-            print(f"{product.name} : {quantity} :{product.price}")
-        print(f"Total price :{self.total_price()}")
+            receipt.append(f"{product.name} : {quantity} : {product.price}")
+        receipt.append(f"Total price : {self.total_price()}")
+        return "\n".join(receipt)
+
+    def __str__(self):
+        return f"Customer({self.first_name} {self.last_name})"
