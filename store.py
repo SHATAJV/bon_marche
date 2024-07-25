72b990acaf5b50029fed65f5de276c77f7b5b3be
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-25 15:54:35 +0200

    modification

--- store.py
+++ store.py
@@ -7,103 +7,49 @@ from stock import Stock
 
 
 class Store:
-    """
-    A class to represent a store.
-
-    Attributes
-    ----------
-
-        A dictionary to store products with product name as the key.
-    customers : list
-        A list to store the customers visiting the store.
-
-    Methods
-    -------
-  
-    find_product(product_name: str) -> Product:
-        Finds and returns a product by its name.
-    add_customer(customer: Customer) -> None:
-        Adds a customer to the store.
-    customer_visit() -> None:
-        Handles the interaction for a customer visiting the store.
-    report() -> str:
-        Generates a daily report of customers and remaining stock.
-    store_menu() -> None:
-        Displays the main menu for store operations.
-    """
-
     def __init__(self):
         self.stock = Stock()
         self.customers: List[Customer] = []
         self.stock.setup_store()
-
+        self.sales_made: bool = False
     def find_product(self, product_name: str) -> Product:
-        """
-        Finds and returns a product by its name.
-
-        Parameters
-        ----------
-        product_name : str
-            The name of the product to find.
-
-        Returns
-        -------
-        Product
-            The product with the specified name, or None if not found.
-        """
-
         return self.stock.find_product(product_name)
-
     def add_customer(self, customer: Customer):
-        """
-        Adds a customer to the store.
-
-        Parameters
-        ----------
-        customer : Customer
-            The customer to be added to the store.
-        """
-
         self.customers.append(customer)
-
+        self.sales_made = True
     def customer_account(self) -> None:
-        """
-        Handles the interaction for a customer visiting the store.
-
-        Prompts the user for their name and the products they wish to purchase,
-        then generates a receipt for the customer.
-        """
         first_name = input("Enter your first name: ")
         last_name = input("Enter your last name: ")
         customer = Customer(first_name, last_name)
         self.add_customer(customer)
-
         while True:
             product_name = input("Enter the product name (or 'done' to finish): ")
             if product_name.lower() == 'done':
                 break
             product = self.find_product(product_name)
             if product:
-                quantity = float(input(f"Enter the quantity of {product_name} : "))
+                quantity = float(input(f"Enter the quantity of {product_name} (in {product.category}): "))
                 customer.add_to_cart(product, quantity)
             else:
                 print("Product not found")
-
         print(customer.receipt())
 
     def report(self) -> str:
-        """
-        Generates a daily report of customers and remaining stock.
-
-        Returns
-        -------
-        str
-            A formatted string containing the daily report.
-        """
-
+        if not self.sales_made:
+            return "You didn't make any sales yet."
         report_lines = ["Daily Report:", "Customers:"]
         for customer in self.customers:
             report_lines.append(str(customer))
-            report_lines.append("Remaining Stock:")
-            report_lines.append(str(self.stock))
-            return "\n".join(report_lines)
+        report_lines.append("Remaining Stock:")
+        for product in self.stock.products.values():
+            report_lines.append(
+                f"{product.name}: Initial Stock: {product.initial_stock}, Current Stock: {product.stock}")
+        return "\n".join(report_lines)
+
+    def report_initial_stock(self):
+        report_lines = ["Initial Stock Report:"]
+        for product in self.stock.products.values():
+            report_lines.append(f"{product.name}: {product.initial_stock}")
+        return "\n".join(report_lines)
+
+
