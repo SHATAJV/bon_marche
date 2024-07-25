e134befb4a8972fcde5ee367e2910e0122a21685
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-07-25 15:41:06 +0200

    correction de menu

--- bon_marche.py
+++ bon_marche.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
-
-
-# -*- coding: utf-8 -*-
 from store import Store
 
 
-def store_menu(setup: Store):
+# -*- coding: utf-8 -*-
+def store_menu(store: Store):
     while True:
-        print("Welcome to the store!")
-        print("1. Customer visit")
-        print("2. Daily report")
-        print("3. Exit")
-        choice = input("Choose an option: ")
+        print("\nStore Menu")
+        print("1. Show Initial Stock")
+        print("2. Report")
+        print("3. Customer Account")
+        print("4. Exit")
+        choice = input("Enter your choice: ")
         if choice == '1':
-            setup.customer_account()
+            print(store.report_initial_stock())
         elif choice == '2':
-            print(setup.report())
+            print(store.report())
+            if not store.sales_made:
+                print("1. View initial stock report")
+                print("3. Exit")
+                sub_choice = input("Enter your choice: ")
+                if sub_choice == '1':
+                    print(store.report_initial_stock())
+                elif sub_choice == '3':
+                    break
         elif choice == '3':
+            store.customer_account()
+        elif choice == '4':
             break
         else:
-            print("Invalid choice, please try again")
-
-
+            print("Invalid choice, please try again.")
 if __name__ == "__main__":
     store = Store()
-    store_menu(store)
